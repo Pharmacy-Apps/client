465f2d16b9df719f621f3d66aa4e14a7d438bcdb
Jerry Edebua <jeredebua@gmail.com>
2019-12-27 10:19:14 +0300

    Added account action to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { IonContent, IonPage } from '@ionic/react'
+import Routes from 'routes'
+import { History } from 'history'
 
+import { IonContent, IonPage } from '@ionic/react'
 import { Header } from 'components'
 
-const Home: React.FC = () => {
+export type Props = {
+  history: History,
+}
+
+const toolbarActions = (history: History) => [{
+  component: 'Account',
+  handler: () => history.push(Routes.account.path)
+}]
+
+const Component: React.FC<Props> = props => {
   return (
     <IonPage>
-      <Header omitsBack title="Requests" />
+      <Header omitsBack title="Requests" actions={toolbarActions(props.history)} />
       <IonContent className="ion-padding">
         Oyster
       </IonContent>
@@ -14,4 +25,4 @@ const Home: React.FC = () => {
   )
 }
 
-export default Home
+export default Component
