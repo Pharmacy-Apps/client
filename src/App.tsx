1f23b5ef5163ea66a3c7fcca64d383c5015f6f90
Jerry Edebua <jeredebua@gmail.com>
2020-01-08 22:08:01 +0300

    Moved styles to a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,7 @@ import { sessionAvailable } from './session'
 
 import { watchPosition as watchUserLocation } from 'location'
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css'
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css'
-import '@ionic/react/css/structure.css'
-import '@ionic/react/css/typography.css'
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css'
-import '@ionic/react/css/float-elements.css'
-import '@ionic/react/css/text-alignment.css'
-import '@ionic/react/css/text-transformation.css'
-import '@ionic/react/css/flex-utils.css'
-import '@ionic/react/css/display.css'
-
-/* Theme variables */
-import './theme/variables.css'
+import 'styles'
 
 export default class App extends React.Component {
 
